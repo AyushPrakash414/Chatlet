53ffcc453669293d2f2f03f199a9cc7aa44a0984
Ayush <prakashayush414@gmail.com>
2025-08-07 14:35:43 +0530

    19:00 added the get and join room method

--- src/main/java/com/ChatLet/chatlet/Controller/RoomController.java
+++ src/main/java/com/ChatLet/chatlet/Controller/RoomController.java
@@ -1,5 +1,9 @@
 package com.ChatLet.chatlet.Controller;
 
+import com.ChatLet.chatlet.Entity.ChatRoom;
+import com.ChatLet.chatlet.Repository.RoomRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,21 @@ public class RoomController
     //create Room
     //get Room
     //get Messages of the Room
+    private RoomRepository roomRepo;
+    public ResponseEntity<?> createRoom(String userRoomId)
+    {
+        if (roomRepo.findByroomId(userRoomId)!=null)
+        {
+            return ResponseEntity.badRequest().body("Room is Already Exist");
+        }
+        else {
+
+            ChatRoom room=new ChatRoom();
+            room.setRoomId(userRoomId);
+            roomRepo.save(room);
+            return ResponseEntity.status(HttpStatus.CREATED).body(room);
+
+        }
+    }
+
 }
