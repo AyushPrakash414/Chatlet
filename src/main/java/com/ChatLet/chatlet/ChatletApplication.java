d36ac36387cd1c8a3c02e52e7ac54dab1cb9c22b
Ayush <prakashayush414@gmail.com>
2025-08-07 12:45:45 +0530

    Intilization of the repo

---
+++ src/main/java/com/ChatLet/chatlet/ChatletApplication.java
@@ -0,0 +1,13 @@
+package com.ChatLet.chatlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChatLet/chatlet/ChatletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChatLet.chatlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
