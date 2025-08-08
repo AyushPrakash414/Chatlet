f94c8f272654120ff9b4b8fa417c51479f606b95
Ayush <prakashayush414@gmail.com>
2025-08-08 22:49:47 +0530

    Adding the getMessages and Join the room Method

--- src/main/java/com/ChatLet/chatlet/Controller/RoomController.java
+++ src/main/java/com/ChatLet/chatlet/Controller/RoomController.java
@@ -1,11 +1,16 @@
 package com.ChatLet.chatlet.Controller;
 
 import com.ChatLet.chatlet.Entity.ChatRoom;
+import com.ChatLet.chatlet.Entity.Messages;
 import com.ChatLet.chatlet.Repository.RoomRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/roomControl")
@@ -29,6 +34,32 @@ public class RoomController
             return ResponseEntity.status(HttpStatus.CREATED).body(room);
 
         }
+    }
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> joinRoom(
+            @PathVariable String roomId
+    )
+
+    {
+        ChatRoom room=roomRepo.findByroomId(roomId);
+        if (room!=null)
+        {
+             return ResponseEntity.ok(room);
+        }
+        return ResponseEntity.badRequest().body("Room not found!!");
+    }
+
+    @GetMapping ("/{roomId}")
+    public ResponseEntity<List<Messages>> getMessages(
+            @PathVariable String roomId
+    )
+    {
+        ChatRoom room=roomRepo.findByroomId(roomId);
+        if (room==null)
+        {
+            return ResponseEntity.badRequest().body(null);
+        }
+        return ResponseEntity.ok().body(room.getMessages());
     }
 
 }
