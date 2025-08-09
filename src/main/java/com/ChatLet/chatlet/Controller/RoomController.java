1b17695636c71232eebfa9f13de0f2248c3f5056
Ayush <prakashayush414@gmail.com>
2025-08-09 23:35:12 +0530

    Adding method to get the msg

--- src/main/java/com/ChatLet/chatlet/Controller/RoomController.java
+++ src/main/java/com/ChatLet/chatlet/Controller/RoomController.java
@@ -18,6 +18,7 @@ public class RoomController
 {
     //create Room
     //get Room
+    //join the room
     //get Messages of the Room
     private RoomRepository roomRepo;
     public ResponseEntity<?> createRoom(String userRoomId)
