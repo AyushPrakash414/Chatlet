575d90322146a3815d4462a9815b3a853d0be9bc
Ayush <prakashayush414@gmail.com>
2025-08-15 23:08:38 +0530

    Adding Config Class for Web Socket

---
+++ src/main/java/com/ChatLet/chatlet/Controller/ChatController.java
@@ -0,0 +1,14 @@
+package com.ChatLet.chatlet.Controller;
+
+import com.ChatLet.chatlet.Entity.Messages;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController
+{
+    public Messages sendMessage()
+    {
+        return null;
+    }
+}
+//Ayush
