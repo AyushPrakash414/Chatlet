3c4c48894d0755b9a0d503acef9f313b3dc9e916
Ayush <prakashayush414@gmail.com>
2025-08-19 22:47:45 +0530

    Adding SendMessage Method

--- src/main/java/com/ChatLet/chatlet/Controller/ChatController.java
+++ src/main/java/com/ChatLet/chatlet/Controller/ChatController.java
@@ -1,11 +1,17 @@
 package com.ChatLet.chatlet.Controller;
 
 import com.ChatLet.chatlet.Entity.Messages;
+import com.ChatLet.chatlet.Repository.RoomRepository;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController
 {
+    private RoomRepository roomRepository;
+    @MessageMapping("/sendMessage/{roomid}")
+    @SendTo("/topic/room/{roomid}")
     public Messages sendMessage()
     {
         return null;
